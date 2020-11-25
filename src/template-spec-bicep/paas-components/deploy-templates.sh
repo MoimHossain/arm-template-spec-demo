0868ca633a07e09712b496bc8f7d317e9af334be
Moim HA <moimhossain@gmail.com>
2020-11-25 12:51:58 +0100

    Added hardcoded info for now

--- src/template-spec-bicep/paas-components/deploy-templates.sh
+++ src/template-spec-bicep/paas-components/deploy-templates.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
+
+export RESOURCE_GROUP="CloudOven-Trusted-TemplateSpecs"
+export LOCATION="westeurope"
+export VERSION="1.0"
+
 echo "Deploying template specs..."
 echo "Resource Group: $RESOURCE_GROUP"
 echo "Location:       $LOCATION"
